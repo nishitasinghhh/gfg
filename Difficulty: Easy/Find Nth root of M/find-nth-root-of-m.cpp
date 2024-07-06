598775605876d2a7ed7b043acfd77acfe6b564a9
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-07-06 15:57:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Nth root of M/find-nth-root-of-m.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	int findPower(int mid,int n,int m)
+	{
+	    long long ans=1;
+	    for(int i=1; i<=n; i++)
+	    {
+	        ans=ans*mid;
+	        if(ans>m)
+	        return 2;
+	    }
+	    if(ans==m) return 1;
+	    return 0;
+	}
+	
+	int NthRoot(int n, int m)
+	{
+	    // Code here.
+	    int low=1;
+	    int high=m;
+	    while(low<=high)
+	    {
+	        int mid=(low+high)/2;
+	        if(findPower(mid,n,m)==1)
+	        return mid;
+	        else if(findPower(mid,n,m)==0)
+	        low=mid+1;
+	        else
+	        high=mid-1;
+	    }
+	    return -1;
+	   
+	}  
+};
+//time complexity:-O(logm +n)
+//space complexity:-o(1)
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		Solution ob;
+		int ans = ob.NthRoot(n, m);
+		cout << ans << "\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
