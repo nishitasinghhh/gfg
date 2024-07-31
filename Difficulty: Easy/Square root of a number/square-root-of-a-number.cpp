581a4e9f15ddd9c0db782bb9556eff0648241556
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-07-31 14:03:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square root of a number/square-root-of-a-number.cpp
+++ Difficulty: Easy/Square root of a number/square-root-of-a-number.cpp
@@ -11,29 +11,21 @@ class Solution{
   public:
     long long int floorSqrt(long long int x) 
     {
-        // Your code goes here   
-      
-        long long low=1;
-        long long high=x;
-        long long ans=1;
+        // Your code goes here  
+        int low=1;
+        int high=x;
         while(low<=high)
         {
-            long long mid=(low+high)/2;
-            long long val=mid*mid;
+            long long int mid=(low+high)/2;
+            long long int val=mid*mid;
             if(val<=x)
-            {
-                ans=mid;
-                low=mid+1;
-            }
+            low=mid+1;
             else
             high=mid-1;
         }
-        return ans;
-
+        return high;
     }
 };
-//time complexity:-O(logn)
-//space complexity:-O(1)
 
 //{ Driver Code Starts.
 
