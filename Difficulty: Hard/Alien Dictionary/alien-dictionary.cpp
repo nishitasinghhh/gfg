c64f69aeb13fc9366bd0e81d19d49d49fc157a43
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-08-24 18:49:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
@@ -7,42 +7,43 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-    public:
-    vector<int> topoSort(vector<int>adj[],int k)
-    {
-        vector<int>indegree(k,0);
-        for(int i=0; i<k; i++)
-        {
-            for(auto it:adj[i])
-            {
-                indegree[it]++;
-            }
-        }
-        queue<int>q;
-        for(int i=0; i<k; i++)
-        {
-            if(indegree[i]==0)
-            q.push(i);
-        }
-        vector<int>topo;
-        while(!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-            topo.push_back(node);
-            for(auto it: adj[node])
-            {
-                indegree[it]--;
-                if(indegree[it]==0) q.push(it);
-            }
-        }
-        return topo;
-    }
-    string findOrder(string dict[], int N, int k) {
-        //code here
+class Solution {
+  public:
+  vector<int>topoSort(vector<int>adj[], int k)
+  {
+      vector<int>indegree(k,0);
+      for(int i=0; i<k; i++)
+      {
+          for(auto it:adj[i])
+          {
+              indegree[it]++;
+          }
+      }
+      queue<int>q;
+      for(int i=0; i<k; i++)
+      {
+          if(indegree[i]==0)
+          q.push(i);
+      }
+      vector<int>topo;
+      while(!q.empty())
+      {
+          int node=q.front();
+          q.pop();
+          topo.push_back(node);
+          for(auto it:adj[node])
+          {
+              indegree[it]--;
+              if(indegree[it]==0)
+              q.push(it);
+          }
+      }
+      return topo;
+  }
+    string findOrder(string dict[], int n, int k) {
+        // code here
         vector<int>adj[k];
-        for(int i=0; i<N-1; i++)
+        for(int i=0; i<n-1; i++)
         {
             string s1=dict[i];
             string s2=dict[i+1];
@@ -55,20 +56,21 @@ class Solution{
                     break;
                 }
             }
+           
         }
-        vector<int> topo=topoSort(adj,k);
-        string ans="";
-        for(auto it:topo)
-        {
-            ans=ans+char(it+'a');
-        }
-        return ans;
+         vector<int>topo=topoSort(adj,k);
+            string ans;
+            for(auto it: topo)
+            {
+                ans=ans+char(it+'a');
+            }
+            return ans;
     }
 };
 
-
 //{ Driver Code Starts.
 string order;
+
 bool f(string a, string b) {
     int p1 = 0;
     int p2 = 0;
@@ -78,7 +80,8 @@ bool f(string a, string b) {
         //	cout<<p1<<" "<<p2<<endl;
     }
 
-    if (p1 == p2 and a.size() != b.size()) return a.size() < b.size();
+    if (p1 == p2 and a.size() != b.size())
+        return a.size() < b.size();
 
     return p1 < p2;
 }
@@ -91,12 +94,14 @@ int main() {
         int N, K;
         cin >> N >> K;
         string dict[N];
-        for (int i = 0; i < N; i++) cin >> dict[i];
-        
+        for (int i = 0; i < N; i++)
+            cin >> dict[i];
+
         Solution obj;
         string ans = obj.findOrder(dict, N, K);
         order = "";
-        for (int i = 0; i < ans.size(); i++) order += ans[i];
+        for (int i = 0; i < ans.size(); i++)
+            order += ans[i];
 
         string temp[N];
         std::copy(dict, dict + N, temp);
@@ -104,10 +109,13 @@ int main() {
 
         bool f = true;
         for (int i = 0; i < N; i++)
-            if (dict[i] != temp[i]) f = false;
+            if (dict[i] != temp[i])
+                f = false;
 
-        if(f)cout << 1;
-        else cout << 0;
+        if (f)
+            cout << 1;
+        else
+            cout << 0;
         cout << endl;
     }
     return 0;
