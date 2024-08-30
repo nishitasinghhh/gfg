b998b7f4d2d3f219373a2230b7acc81c70671581
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-08-31 02:49:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Largest odd number in string/largest-odd-number-in-string.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string maxOdd(string s) {
+        // your code here
+        int n=s.size();
+        for(int i=n-1; i>=0; i--)
+        {
+            if((s[i]-'0')%2!=0)
+            return s.substr(0,i+1);
+        }
+        return "";
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+
+        cout << ob.maxOdd(s) << endl;
+    }
+}
+
+// } Driver Code Ends
