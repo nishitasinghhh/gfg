c918af4af158e0c6aafe639b7e26ca43f57e4a4b
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-08-31 22:28:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Geek Jump/geek-jump.cpp
+++ Difficulty: Easy/Geek Jump/geek-jump.cpp
@@ -6,22 +6,24 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int f(int index, vector<int>&height,vector<int>&dp)
+  int f(int index, vector<int> &height,vector<int> &dp)
   {
       if(index==0) return 0;
       if(dp[index]!=-1) return dp[index];
-      int left=f(index-1, height,dp)+abs(height[index]-height[index-1]);
+      int left=f(index-1,height,dp)+abs(height[index]-height[index-1]);
       int right=INT_MAX;
       if(index>1)
-      right=f(index-2, height,dp)+abs(height[index]-height[index-2]);
-      return dp[index]= min(right,left);
+      right=f(index-2, height,dp) +abs(height[index]-height[index-2]);
+      return dp[index]=min(left,right);
   }
     int minimumEnergy(vector<int>& height, int n) {
         // Code here
-        vector<int>dp(n+1,-1);
-        return f(n-1,height,dp);
+       vector<int>dp(n+1,-1);
+ return f(n-1,height,dp);
     }
 };
+//time complexity:-O(N)
+//space complexity:-O(1) array 
 
 //{ Driver Code Starts.
 
