51562bd9c9244401b01533e4ebadc39b9c7c3456
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-10-13 14:13:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Implement stack using array/implement-stack-using-array.cpp
+++ Difficulty: Basic/Implement stack using array/implement-stack-using-array.cpp
@@ -1,51 +1,22 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class MyStack
-{
-private:
+class MyStack {
+  private:
     int arr[1000];
     int top;
-public:
-    MyStack(){top=-1;}
+
+  public:
+    MyStack() { top = -1; }
+
     int pop();
     void push(int);
 };
 
 
-int main()
-{
-
-    int T;
-    cin>>T;
-    while(T--)
-    {
-        MyStack *sq = new MyStack();
-
-        int Q;
-        cin>>Q;
-        while(Q--){
-        int QueryType=0;
-        cin>>QueryType;
-        if(QueryType==1)
-        {
-            int a;
-            cin>>a;
-            sq->push(a);
-        }else if(QueryType==2){
-            cout<<sq->pop()<<" ";
-
-        }
-        }
-        cout<<endl;
-    }
-}
-
 // } Driver Code Ends
-
-
-//Function to push an integer into the stack.
+// Function to push an integer into the stack.
 
 /*
 class MyStack
@@ -60,20 +31,54 @@ public:
 };
 */
 
-void MyStack :: push(int x)
-{
+void MyStack ::push(int x) {
     // Your Code
-    top++;
+    top=top+1;
     arr[top]=x;
 }
 
-//Function to remove an item from top of the stack.
-int MyStack :: pop()
-{
-    // Your Code   
+// Function to remove an item from top of the stack.
+int MyStack ::pop() {
+    // Your Code
     if(top==-1)
     return -1;
-    int res=arr[top];
-    top--;
-    return res;
+    int el=arr[top];
+    top=top-1;
+    return el;
 }
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore(); // Flush the newline character left by cin
+    while (T--) {
+        MyStack *sq = new MyStack();
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        int n = nums.size();
+        int i = 0;
+        while (i < n) {
+            int QueryType = nums[i++];
+            if (QueryType == 1) {
+                int a = nums[i++];
+                sq->push(a);
+            } else if (QueryType == 2) {
+                cout << sq->pop() << " ";
+            }
+        }
+        cout << endl;
+        delete sq;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
