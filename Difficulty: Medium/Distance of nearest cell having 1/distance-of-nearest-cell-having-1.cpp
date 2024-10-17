8f12079ef5fbb953163acff7e4c89d40e337cd66
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-10-18 01:51:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
+++ Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
@@ -7,9 +7,9 @@ using namespace std;
 class Solution {
   public:
     // Function to find distance of nearest 1 in the grid for each cell.
-    void bfs(vector<vector<int>>& grid,vector<vector<int>>& vis,vector<vector<int>>& dis)
+    void findDistance(vector<vector<int>>& grid,vector<vector<int>> &vis,vector<vector<int>> &dis)
     {
-         int n=grid.size();
+        int n=grid.size();
         int m=grid[0].size();
         queue<pair<pair<int,int>,int>>q;
         for(int i=0; i<n; i++)
@@ -18,28 +18,28 @@ class Solution {
             {
                 if(grid[i][j]==1)
                 {
-                    q.push({{i,j},0});
                     vis[i][j]=1;
+                    q.push({{i,j},0});
                 }
             }
         }
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
         while(!q.empty())
         {
             int r=q.front().first.first;
             int c=q.front().first.second;
             int step=q.front().second;
-            q.pop();
             dis[r][c]=step;
-            int drow[]={-1,0,1,0};
-            int dcol[]={0,1,0,-1};
+            q.pop();
             for(int i=0; i<4; i++)
             {
                 int nrow=r+drow[i];
                 int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=1)
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] )
                 {
-                    q.push({{nrow,ncol},step+1});
                     vis[nrow][ncol]=1;
+                    q.push({{nrow,ncol},step+1});
                 }
             }
         }
@@ -50,7 +50,7 @@ class Solution {
         int m=grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
         vector<vector<int>>dis(n,vector<int>(m,0));
-        bfs(grid,vis,dis);
+        findDistance(grid,vis,dis);
         return dis;
     }
 };
