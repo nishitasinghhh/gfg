a0dfe1131ec855e6e4f70e927e7b18c8a03c53b1
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2025-01-07 22:40:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.cpp
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -6,35 +6,32 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int f(int day, int last, vector<vector<int>>& points,vector<vector<int>>&dp)
-  {
-      if(day==0)
-      {int maxi=0;
-          for(int task=0; task<3; task++)
-          {
-              if(task!=last)
-              {
-                  maxi=max(maxi,points[0][task]);
-              }
-          }
-                return maxi;
-      }
-if(dp[day][last]!=-1) return dp[day][last];
-      int maxi=0;
-      for(int task=0; task<3; task++)
-      {
-          if(task!=last)
-          {
-              int point=points[day][task]+f(day-1,task,points,dp);
-              maxi=max(maxi,point);
-          }
-      }
-      return dp[day][last]= maxi;
-  }
-    int maximumPoints(vector<vector<int>>& points, int n) {
+  
+    int maximumPoints(vector<vector<int>>& arr, int n) {
         // Code here
-        vector<vector<int>>dp(n,vector<int>(4,-1));
-        return f(n-1,3,points,dp);
+        vector<vector<int>>dp(n,vector<int>(4,0));
+       dp[0][0]=max(arr[0][1],arr[0][2]);
+       dp[0][1]=max(arr[0][0],arr[0][2]);
+       dp[0][2]=max(arr[0][0],arr[0][1]);
+       dp[0][3]=max(arr[0][0],max(arr[0][1],arr[0][2]));
+       for(int day=1; day<n; day++)
+       {
+           for(int last=0; last<4; last++)
+           {
+              dp[day][last]=0;
+             
+              for(int task=0; task<3; task++)
+              {
+                  if(task!=last)
+                  {
+                      int point=arr[day][task]+dp[day-1][task];
+                      dp[day][last]=max(dp[day][last],point);
+                  }
+              }
+              
+           }
+       }
+       return dp[n-1][3];
     }
 };
 
