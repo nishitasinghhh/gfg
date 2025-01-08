9641c91e0e3b9f5aa6757afa58dce636f52437d5
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2025-01-09 02:09:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Chocolates Pickup/chocolates-pickup.cpp
+++ Difficulty: Hard/Chocolates Pickup/chocolates-pickup.cpp
@@ -6,41 +6,38 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int maxChocoUtil(int i,int j1, int j2, int n, int m, vector<vector<int>>& grid,   vector<vector<vector<int>>> &dp)
+  int findPath(int i, int j1, int j2, int n, int m,vector<vector<int>>& grid,
+  vector<vector<vector<int>>> &dp)
   {
-      if (j1 < 0 || j1 >= m || j2 < 0 || j2 >= m)
-        return -1e9;
-        if(i==n-1) 
-        {
-            if(j1==j2) return grid[i][j1];
-            else return grid[i][j1]+grid[i][j2];
-            
-        }
-        if (dp[i][j1][j2] != -1)
-        return dp[i][j1][j2];
-           int maxi = INT_MIN;
-           for(int dj1=-1; dj1<=1; dj1++)
-           {
-               for(int dj2=-1; dj2<=1; dj2++)
-               { int ans;
-                   if (j1 == j2)
-                ans = grid[i][j1] + maxChocoUtil(i + 1, j1 + dj1, j2 + dj2, n, m, grid, dp);
-            else
-                ans = grid[i][j1] + grid[i][j2] + maxChocoUtil(i + 1, j1 + dj1, j2 + dj2, n, m, grid, dp);
-                maxi=max(maxi,ans);
-            
-               }
-           }
-           return dp[i][j1][j2] = maxi;
+      if(j1<0 || j1>=m || j2<0 || j2>=m) return -1e9;
+      if(i==n-1)
+      {
+          if(j1==j2) return grid[i][j1];
+          else return grid[i][j1]+grid[i][j2];
+      }
+      if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+      int maxi=INT_MIN;
+      for(int dj1=-1;  dj1<=1; dj1++)
+      {
+          for(int dj2=-1; dj2<=1; dj2++)
+          {
+              int val;
+              if(j1==j2)
+              val=grid[i][j1];
+              else
+              val=grid[i][j1]+grid[i][j2];
+              val+=findPath(i+1, j1+dj1, j2+dj2,  n,  m, grid,dp);
+              maxi=max(maxi,val);
+          }
+      }
+      return dp[i][j1][j2]= maxi;
   }
     int solve(int n, int m, vector<vector<int>>& grid) {
         // code here
         vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return maxChocoUtil(0, 0, m - 1, n, m, grid, dp);
+        return findPath(0,0,m-1,n,m,grid,dp);
     }
 };
-//time complexity:-O(N*M*M)*9
-//space complexity:-O(N*M*M)+O(N)
 
 //{ Driver Code Starts.
 
@@ -63,6 +60,8 @@ int main() {
 
         Solution obj;
         cout << obj.solve(n, m, grid) << "\n";
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
