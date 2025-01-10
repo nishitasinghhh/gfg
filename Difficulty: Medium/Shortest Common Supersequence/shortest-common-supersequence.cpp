3512bbcf71fec42dd9b1bd923fb4862191bb40cd
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2025-01-10 23:56:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -1,63 +1,66 @@
 //{ Driver Code Starts
-//Initial template for C++
+// Initial template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-//User function template for C++
+// User function template for C++
 
-
-class Solution
-{
-    public:
-    int lcs(string &x, string &y)
+class Solution {
+  public:
+    // Function to find length of shortest common supersequence of two strings
+    int lcs(string &s1, string &s2)
     {
-        int n=x.size();
-        int m=y.size();
+        int n=s1.size();
+        int m=s2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0; i<=m; i++) dp[0][i]=0;
         for(int i=0; i<=n; i++) dp[i][0]=0;
-        for(int j=0; j<=m; j++) dp[0][j]=0;
         for(int i=1; i<=n; i++)
         {
             for(int j=1; j<=m; j++)
             {
-                if(x[i-1]==y[j-1])
-                dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
         return dp[n][m];
     }
-    //Function to find length of shortest common supersequence of two strings.
-    int shortestCommonSupersequence(string X, string Y, int m, int n)
-    {
-        //code here
-        return m+n-lcs(X,Y);
+    int shortestCommonSupersequence(string &s1, string &s2) {
+        // code here
+        int n=s1.size();
+        int m=s2.size();
+        return (n+m)-lcs(s1,s2);
     }
+    
 };
 
 //{ Driver Code Starts.
 
-int main()
-{   
-    
-    int t;
-    
-    //taking total testcases
-    cin >> t;
-    while(t--){
-    string X, Y;
-    //taking String X and Y
-	cin >> X >> Y;
-	
-	//calling function shortestCommonSupersequence()
-	Solution obj;
-	cout << obj.shortestCommonSupersequence(X, Y, X.size(), Y.size())<< endl;
-    }
-	return 0;
-}
+int main() {
 
+    int t;
+
+    // taking total testcases
+    cin >> t;
+    while (t--) {
+        string X, Y;
+        // taking String X and Y
+        cin >> X >> Y;
+
+        // calling function shortestCommonSupersequence()
+        Solution obj;
+        cout << obj.shortestCommonSupersequence(X, Y) << endl;
+    }
+    return 0;
+}
 
 // } Driver Code Ends
