35f276369d4e7d1bbdb88e376562a6a0c2da1745
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2025-01-13 11:19:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -8,23 +8,29 @@ using namespace std;
 
 class Solution {
   public:
-  int findSteps(int i, int j, vector<int> &arr, vector<vector<int>> &dp)
-  {
-      if(i==j) return 0;
-      if(dp[i][j]!=-1) return dp[i][j];
-      int mini=1e9;
-      for(int k=i; k<j; k++)
-      {
-          int steps=arr[i-1]*arr[k]*arr[j]+findSteps(i,k,arr,dp)+findSteps(k+1,j,arr,dp);
-          mini=min(mini,steps);
-      }
-      return dp[i][j]= mini;
-  }
+ 
     int matrixMultiplication(vector<int> &arr) {
         // code here
         int n=arr.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return findSteps(1,n-1,arr,dp);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=1; i<n; i++)
+        {
+            dp[i][i]=0;
+        }
+        for(int i=n-1; i>=1; i--)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                int mini=1e9;
+      for(int k=i; k<j; k++)
+      {
+          int steps=arr[i-1]*arr[k]*arr[j]+dp[i][k] + dp[k+1][j];
+          mini=min(mini,steps);
+      }
+       dp[i][j]= mini;
+            }
+        }
+        return dp[1][n-1];
     }
 };
 
